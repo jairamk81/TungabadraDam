70edce9622c3cdff8fd2bc0d9e060d4b2c625538
jairamk8 <jairamk8@hotmail.com>
2017-08-24 10:39:36 -0400

    TungabadraDam

---
+++ app/src/main/java/com/jairam/level/MainActivity.java
@@ -0,0 +1,38 @@
+package com.jairam.level;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myweb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setpage();
+
+    }
+
+    public void setpage(){
+
+        myweb = (WebView) findViewById(R.id.myViewId);
+        WebSettings mysettings= myweb.getSettings();
+        mysettings.setJavaScriptEnabled(true);
+        myweb.loadUrl("http://tbboard.in/Reservoir/ReservoirDataViewa.php");
+        myweb.setWebViewClient(new WebViewClient());
+    }
+
+    public void onBackPressed(){
+
+        if(myweb.canGoBack())
+            myweb.goBack();
+        else
+        super.onBackPressed();
+
+    }
+}
