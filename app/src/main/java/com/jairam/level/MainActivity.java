7f1af22adc74b920a427d36eb62f45566e48145c
jairamk8 <jairamk8@hotmail.com>
2017-08-29 12:43:31 -0400

    TungbadraDam Final Version 1.2~App URL Update

--- app/src/main/java/com/jairam/level/MainActivity.java
+++ app/src/main/java/com/jairam/level/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         myweb = (WebView) findViewById(R.id.myViewId);
         WebSettings mysettings= myweb.getSettings();
         mysettings.setJavaScriptEnabled(true);
-        myweb.loadUrl("http://tbboard.in/Reservoir/ReservoirDataViewa.php");
+        myweb.loadUrl("http://tungabhadraboard.in/tbbrsms/reservoir_info_report.php");
         myweb.setWebViewClient(new WebViewClient());
     }
 
